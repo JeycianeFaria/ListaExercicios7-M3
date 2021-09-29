132875ac553c28f93a5fdbd9b6334496b23018fe
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-09-29 08:38:22 -0300

    Alterando para  a classe correta  os métodos anteriores

--- src/br/com/zup/PratoDoDia.java
+++ src/br/com/zup/PratoDoDia.java
@@ -63,23 +63,5 @@ public class PratoDoDia {
         return retorno.toString();
     }
 
-    //Menu
-    public static void menu() {
-        System.out.println("======Bem vindo ao Restaurante Mineiro======");
-        System.out.println("1- Cadastrar Prato.");
-        System.out.println("2- Cadastrar Ingrediente");
-        System.out.println("3-Adicionar Ingredientes aos Pratos");
-        System.out.println("4- Exibir Cardápio");
-        System.out.println("5- Sair");
-    }
-
-    //Cadastrar Prato
-    public static PratoDoDia cadastrarPrato() {
-        String nome = capturarDados("Digite o nome do Prato: ").nextLine();
-        double valor = capturarDados("Digite o valor do do prato: ").nextDouble();
-
-        PratoDoDia prato = new PratoDoDia(nome, valor);
-        return prato;
-    }
 
 }
--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -12,5 +12,31 @@ public class Sistema {
         return new Scanner(System.in);
     }
 
+    //Menu
+    public static void menu() {
+        System.out.println("======Bem vindo ao Restaurante Mineiro======");
+        System.out.println("1- Cadastrar Prato.");
+        System.out.println("2- Cadastrar Ingrediente");
+        System.out.println("3-Adicionar Ingredientes aos Pratos");
+        System.out.println("4- Exibir Cardápio");
+        System.out.println("5- Sair");
+    }
+
+    //Cadastrar Prato
+    public static PratoDoDia cadastrarPrato() {
+        String nome = capturarDados("Digite o nome do Prato: ").nextLine();
+        double valor = capturarDados("Digite o valor do do prato: ").nextDouble();
+
+        PratoDoDia prato = new PratoDoDia(nome, valor);
+        return prato;
+    }
+
+    //Cadastrar Ingrediente
+    public static  Ingrediente cadastrarIngrediente(){
+        String nome = capturarDados("Digite o nome do ingrediente: ").nextLine();
+
+        Ingrediente ingrediente = new Ingrediente(nome);
+        return  ingrediente;
+    }
 
 }
